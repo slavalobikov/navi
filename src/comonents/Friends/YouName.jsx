33de2dd877c1ce0b3cca0b9d9838f82ff1ba5e10
lobik <lobikova12@mail.ru>
2020-03-15 16:53:06 +0300

    Friends and fix messages

---
+++ src/comonents/Friends/YouName.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+/*import cls from './../Dialogs.module.css';*/
+import {NavLink} from "react-router-dom";
+
+
+
+const YouName = (props) => {
+
+
+    return <div>
+
+
+    </div>
+};
+
+
+export default YouName;
